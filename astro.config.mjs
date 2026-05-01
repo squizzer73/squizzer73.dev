88980cf2d7c05b33eaaee0dadb5d68cdd8a777e2
squizzer <mark.squires@icloud.com>
2026-05-01 21:31:29 +0100

    chore: remove redundant cloudflare adapter (static output), add astro check script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'static',
-  adapter: cloudflare(),
   integrations: [mdx()],
   site: 'https://squizzer73.dev',
 });
